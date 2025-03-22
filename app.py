270b15325b4eecd8e2c7014b338755366d7c7de7
GiriprasadSirasanambeti <giriprasadsirasanambeti@gmail.com>
2025-03-22 14:29:54 +0530

    Update app.py to use dynamic port for Render deployment

--- app.py
+++ app.py
@@ -43,4 +43,4 @@ def index():
     return render_template('index.html', total_sales=total_sales)
 
 if __name__=='__main__':
-    app.run(debug=True, port=5001)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT',5001)),debug=False)
